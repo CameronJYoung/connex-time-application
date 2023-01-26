c7ed48910cb22ddbae9c97029bd0a7b46ba96e9c
CameronJYoung <cameronjyoung123@gmail.com>
2023-01-26 23:28:44 +0000

    feat: created epoch controller for time route

---
+++ api/src/controllers/EpochController.ts
@@ -0,0 +1,33 @@
+import { Request, Response } from 'express';
+
+import IController from '../interfaces/IController';
+import IEpochService from '../interfaces/IEpochService';
+import Route from '../types/Route';
+
+class EpochController implements IController {
+  public basePath: string = '/time';
+  private epochService: IEpochService;
+
+  constructor(epochService: IEpochService) {
+    this.epochService = epochService;
+  }
+
+  public timeHandler = async (req: Request, res: Response) => {
+    const epochSeconds = this.epochService.generateEpoch();
+    
+    res.status(200).json({
+      epoch: epochSeconds
+    })
+  }
+  
+  public routes: Route[] = [
+    {
+      middleware: [],
+      path: '/',
+      handler: this.timeHandler,
+      methodType: 'GET'
+    }
+  ];
+}
+
+export default EpochController;
