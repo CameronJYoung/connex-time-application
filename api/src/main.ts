89d76404a25a61b0de8196c4fadf92d12b16a4fe
CameronJYoung <cameronjyoung123@gmail.com>
2023-01-26 23:29:50 +0000

    feat: added epoch service and controller to main

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,24 +1,29 @@
 import prometheus from 'express-prometheus-middleware';
 
-import HomeController from './controllers/HomeController';
-import Router from './utils/Router';
 import Server from './utils/Server';
+import Router from './utils/Router';
 import prometheusConfig from './config/prometheusConfig';
+import HomeController from './controllers/HomeController';
+import EpochService from './services/EpochService';
+import EpochController from './controllers/EpochController';
+import authenticateRequest from './middleware/authenticateRequest';
 
 async function main() {
-  
+  // Init services
+  const epochService = new EpochService();
 
   // Init controllers
   const homeController = new HomeController();
+  const epochController = new EpochController(epochService);
 
   // Init server
   const prometheusApp = prometheus(prometheusConfig);
-  const router = new Router([homeController]);
-  const server = new Server([], router, prometheusApp);
+  const router = new Router([epochController, homeController]);
+  const server = new Server([authenticateRequest], router, prometheusApp);
 
   server.start();
 }
 
 main().catch((err) => {
-  console.log(`API Error: ${JSON.stringify(err)}`);
+  console.error(`API Error: ${JSON.stringify(err)}`);
 })
