34a6dff29a5ad6e20f8274cd0ac650e21774380a
CameronJYoung <cameronjyoung132@gmail.com>
2023-01-30 17:07:53 +0000

    feat: added some styles and added hook for metrics

--- frontend/src/components/ResponseMenu/ResponseMenu.styles.tsx
+++ frontend/src/components/ResponseMenu/ResponseMenu.styles.tsx
@@ -3,5 +3,8 @@ import { styled } from '@stitches/react';
 export const Wrapper = styled('section', {
   width: '50%',
   height: '100%',
-  background: 'blue'
+  overflowY: 'scroll',
+  overflowX: 'scroll'
 });
+
+export const FormattedMetrics = styled('pre')
--- frontend/src/components/ResponseMenu/index.tsx
+++ frontend/src/components/ResponseMenu/index.tsx
@@ -1,17 +1,21 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
-import { Wrapper } from './ResponseMenu.styles';
+import { Wrapper, FormattedMetrics } from './ResponseMenu.styles';
+import useApiData from '../../hooks/useApiData';
 
 type Props = {}
 
 function ResponseMenu({}: Props) {
+  const { data, isLoading } = useApiData('http://localhost:8080/metrics');
 
   return (
     <Wrapper>
       <h1>Analytics</h1>
-      <pre>
-
-      </pre>
+      <FormattedMetrics>
+        {
+          data && data.data
+        }
+      </FormattedMetrics>
     </Wrapper>
   )
 }
