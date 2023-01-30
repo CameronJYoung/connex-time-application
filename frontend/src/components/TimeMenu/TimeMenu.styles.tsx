3bc2c6ef5f5dacd4ae2605db1a3af211855533ea
CameronJYoung <cameronjyoung132@gmail.com>
2023-01-30 17:08:12 +0000

    feat: added basic styling and epoch handling

--- frontend/src/components/TimeMenu/TimeMenu.styles.tsx
+++ frontend/src/components/TimeMenu/TimeMenu.styles.tsx
@@ -2,6 +2,5 @@ import { styled } from '@stitches/react';
 
 export const Wrapper = styled('section', {
   width: '50%',
-  height: '100%',
-  background: 'red'
+  height: '100%'
 });
--- frontend/src/components/TimeMenu/index.tsx
+++ frontend/src/components/TimeMenu/index.tsx
@@ -1,16 +1,24 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import { Wrapper } from './TimeMenu.styles';
+import useApiData from '../../hooks/useApiData';
+import currentEpochSeconds from '../../utils/currentEpochSeconds';
+import epochToTimeDiff from '../../utils/epochToTimeDiff';
+import useEpochDiff from '../../hooks/useEpochDiff';
 
 type Props = {}
 
 function TimeMenu({}: Props) {
+  const { data, isLoading } = useApiData('http://localhost:8080/time');
+  const epochDifference = useEpochDiff(data);
+  
   return (
     <Wrapper>
       <h1>Times</h1>
       <div>
-        <span>Most recent epoch</span>
-        <span>Difference between client time and server time</span>
+        <span>Most recent epoch: {data && data.epoch}</span><br />
+        <span>Difference between client time and server time: {!isLoading && epochDifference}</span><br />
+        <span>{ isLoading && 'Loading...' }</span>
       </div>
     </Wrapper>
   )
